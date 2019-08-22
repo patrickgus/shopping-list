b39bb4d6c544ff4888c80fd593aaa6665ed07247
Patrick Gustafson <patrickgus@charter.net>
2019-08-22 08:18:15 -0700

    modified check function

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function shoppingList() {
   });
 
   $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
   });
 
   $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
